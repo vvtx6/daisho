1350812b289c1fdfd4316895affee23f69cb0f10
vvtx6 <tkuramoto0630@gmail.com>
2025-10-16 15:42:49 +0900

    2025/10/16

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -1,5 +1,5 @@
-const helloBtn = document.getElementByld("helloBtn")
-const message = document.getElementByld("message")
+const helloBtn = document.getElementByld("helloBtn");
+const message = document.getElementByld("message");
 
 helloBtn.addEventListener("click",() => {
     const hour = new Date().getHours();
